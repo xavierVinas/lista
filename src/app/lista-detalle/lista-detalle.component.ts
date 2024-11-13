92a8a65eaef73a4b26cee9599f20759059d44983
xavierVinas <xavivinass@gmail.com>
2024-11-13 10:27:05 +0100

    Added checkboxes to list items in 'ListaDetalle'

--- src/app/lista-detalle/lista-detalle.component.ts
+++ src/app/lista-detalle/lista-detalle.component.ts
@@ -1,52 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { ActivatedRoute } from '@angular/router';
-// import { ListasService } from '../listas.service';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { HeaderComponent } from '../header/header.component';
-
-// @Component({
-//   selector: 'app-lista-detalle',
-//   standalone: true,
-//   imports: [CommonModule, FormsModule, HeaderComponent],
-//   templateUrl: './lista-detalle.component.html',
-//   styleUrls: ['./lista-detalle.component.scss'],
-// })
-// export class ListaDetalleComponent implements OnInit {
-//   lista: { id: number; nombre: string; items: string[] } | undefined;
-//   nuevoItem: string = '';
-//   username: string = '';
-
-//   constructor(
-//     private route: ActivatedRoute,
-//     private listasService: ListasService,
-//     private router: Router
-//   ) {}
-
-//   ngOnInit() {
-//     this.username = localStorage.getItem('username') ?? 'Usuario';
-//     const id = Number(this.route.snapshot.paramMap.get('id'));
-//     this.listasService.obtenerListas().subscribe((listas) => {
-//       this.lista = listas.find((lista) => lista.id === id);
-//     });
-//   }
-
-//   agregarItem() {
-//     if (this.nuevoItem.trim() && this.lista) {
-//       this.listasService.agregarItemALista(this.lista.id, this.nuevoItem);
-//       this.nuevoItem = '';
-//     }
-//   }
-
-//   volverAListas() {
-//     this.router.navigate(['/listas']);
-//   }
-
-//   onLogout() {
-   
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
@@ -90,8 +41,8 @@ export class ListaDetalleComponent implements OnInit {
   agregarItem() {
     if (this.nuevoItem.trim() && this.lista) {
       const nuevoElemento = { texto: this.nuevoItem, completado: false };
-      this.lista.items.push(nuevoElemento); // Añadir el nuevo elemento a la lista
-      this.nuevoItem = ''; // Limpiar el campo de entrada
+      this.lista.items.push(nuevoElemento);
+      this.nuevoItem = ''; 
     }
   }
 
@@ -104,7 +55,7 @@ export class ListaDetalleComponent implements OnInit {
   }
 
   onLogout() {
-    // Lógica para cerrar sesión
+
   }
 }
 
