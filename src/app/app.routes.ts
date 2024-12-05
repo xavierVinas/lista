8b1fe13841348329f18f77f29d8c8abf70670681
hconde <hconde>
2024-12-05 13:27:00 +0100

    refactor routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,27 @@ import { RegisterComponent } from './features/auth/register/register.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { ListasComponent } from './features/list/views/listas/listas.component';
 import { ListaDetalleComponent } from './features/list/views/lista-detalle/lista-detalle.component';
+import { HomeComponent } from './features/home/views/home/home.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'listas', component: ListasComponent, canActivate: [AuthGuard] },
   {
-    path: 'lista/:id',
-    component: ListaDetalleComponent,
+    path: '',
+    component: HomeComponent,
     canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'listas',
+        pathMatch: 'full',
+      },
+      { path: 'listas', component: ListasComponent },
+      {
+        path: 'lista/:id',
+        component: ListaDetalleComponent,
+      },
+    ],
   },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
 ];
